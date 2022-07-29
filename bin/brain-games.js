a2295355cbf935b6ce8c1313a3498921240e56ab
Vadim Abramov <Tvelse410@yandex.ru>
2022-07-29 22:57:35 +0400

    add eslint, makefile, commands to brain.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
-import helloQuestion from "../src/cli.js";
+import helloQuestion from '../src/cli.js';
+
 console.log('Welcome to the Brain Games');
 console.log(helloQuestion());
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
+
 const helloQuestion = () => {
-    console.log('May i have your name?');
-    const name = readlineSync.question('Your answer: ');
-    console.log(`Hello, ${name}!`);
+  console.log('May i have your name?');
+  const name = readlineSync.question('Your answer: ');
+  console.log(`Hello, ${name}!`);
 };
 export default helloQuestion;
